168d7535b39002b0efd7255415e392c106c984ab
stopdaydreaming <hello@stopdaydreaming.com>
2020-12-30 23:17:08 -0500

    localstorage working, added styling

--- script.js
+++ script.js
@@ -2,28 +2,38 @@ $(document).ready(function() {
   //getItem
   // parse: converts back to object
   var itemsArray = JSON.parse(localStorage.getItem("items")) || [];
-
+  console.log(itemsArray);
+  for (var i = 0; i < itemsArray.length; i++) {
+    var itemText = itemsArray[i];
+    generateList(itemText);
+  }
   $("#add-items").on("submit", function(e) {
     e.preventDefault();
-    // console.log("form clicked");
-    var itemEl = $("#item");
+    var itemEl = $("#wish-item");
     var itemText = itemEl.val();
-    var listEl = $("<li>");
-    listEl.append(itemText);
-    var list = $("#list-items");
-    var delItem = $("<button>");
-    delItem.text("Delete");
-    delItem.addClass("btn btn-danger");
-    delItem.on("click", function() {
-      $(listEl).remove();
-    });
-    list.append(listEl);
-    listEl.append(delItem);
-
+    generateList(itemText);
     itemsArray.push(itemText);
 
     //setItem
     // converts to string
     localStorage.setItem("items", JSON.stringify(itemsArray));
   });
+
+  function generateList(itemText) {
+    var listEl = $("<li>");
+    listEl.append(itemText);
+    var list = $("#list-items");
+    //delete button
+    var delItem = $("<button>");
+    delItem.text("remove");
+    delItem.addClass("btn btn-danger");
+    delItem.on("click", function() {
+      $(listEl).remove();
+    });
+
+    list.append(listEl);
+    listEl.append(delItem);
+    var hrEl = $("<hr>");
+    listEl.after(hrEl);
+  }
 });
